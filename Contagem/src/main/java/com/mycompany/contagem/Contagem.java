d09c9b9c052664d226eb75c95ddfbca1163c6a0f
EduardoGoncalves-02 <goncalvespereira02edu@gmail.com>
2026-04-02 00:26:19 -0300

    Exercícios de looping for

---
+++ Contagem/src/main/java/com/mycompany/contagem/Contagem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.contagem;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Contagem {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++){
+            JOptionPane.showMessageDialog(null, i);
+        }
+    }
+}
