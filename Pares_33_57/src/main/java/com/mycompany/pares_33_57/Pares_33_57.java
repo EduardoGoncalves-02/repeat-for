c0cb2642d78bbc352c6c51a60b99d8505837d123
EduardoGoncalves-02 <goncalvespereira02edu@gmail.com>
2026-04-02 00:26:55 -0300

    Exercícios de looping for - pares entra 33 e 57

---
+++ Pares_33_57/src/main/java/com/mycompany/pares_33_57/Pares_33_57.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pares_33_57;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Pares_33_57 {
+
+    public static void main(String[] args) {
+        for(int i = 34; i < 57; i = i + 2){
+            JOptionPane.showMessageDialog(null,i);
+        }
+    }
+}
